20067c613905b10cc8421cec1637300de4679dcd
Anna Goldberg <annalexgoldberg@gmail.com>
2016-01-29 14:05:24 -0500

    adds functions for request and botfactory

--- plugin.py
+++ plugin.py
@@ -3,12 +3,12 @@ import json
 import requests
 from flask import Blueprint, request
 
-from DivvyPlugins.hookpoints import hookpoint
-from DivvyPlugins.plugin_helpers import register_api_blueprint, unregister_api_blueprints
+from DivvyPlugins.hookpoints      import hookpoint
+from DivvyPlugins.plugin_helpers  import register_api_blueprint, unregister_api_blueprints
 from DivvyPlugins.plugin_metadata import PluginMetadata
-from DivvyUtils.flask_helpers import JsonResponse
-from DivvyResource.Resources import DivvyPlugin
-from DivvyPlugins.settings import GlobalSetting
+from DivvyUtils.flask_helpers     import JsonResponse
+from DivvyResource.Resources      import DivvyPlugin
+from DivvyPlugins.settings        import GlobalSetting
 
 
 class metadata(PluginMetadata):
@@ -29,6 +29,9 @@ PREFIX   = DivvyPlugin.get_current_plugin().name
 APIKEY   = PREFIX + '.apiKey'
 CHANNEL  = PREFIX + '.channel'
 USERNAME = PREFIX + '.username'
+MESSAGE  = PREFIX + '.message'
+#Plugin reference
+PLUGIN_REF = DivvyPlugin.get_current_plugin()
 
 #Register Global Settings
 apiKey = GlobalSetting(
@@ -49,42 +52,87 @@ username = GlobalSetting(
     type_hint    = 'string',
     description  = 'This will be the username that messages are sent from')
 
+message = GlobalSetting(
+    name         = MESSAGE,
+    display_name = 'Messagy body',
+    type_hint    = 'string',
+    description  = 'This is a default message body'
+)
 
-@blueprint.route('/', methods=['POST'])
-def send_to_slack():
+def send_to_slack(channel,username,message,apiKey):
     """Send a message to slack."""
-    #get message
-    message = json.loads(request.data)
-
-    #get plugin reference
-    plugin_ref = DivvyPlugin.get_current_plugin();
 
     # Form slack payload
     payload = {
-        'channel': channel.get_for_resource(plugin_ref),
-        'username': username.get_for_resource(plugin_ref),
-        'text': message['text']
+        'channel': channel,
+        'username': username,
+        'text': message
     }
 
     data = json.dumps(payload)
+    response = requests.post(apiKey, data=data)
+    return response.content
 
-    response = requests.post(apiKey.get_for_resource(plugin_ref), data=data)
+@blueprint.route('/', methods=['POST'])
+def send_slack():
+    """Send a message to slack by request"""
+    payload        = json.loads(request.data)
+    channel_name   = payload.get('channel', channel.get_for_resource(PLUGIN_REF))
+    user_name      = payload.get('username', username.get_for_resource(PLUGIN_REF))
+    message_body   = payload.get('text', message.get_for_resource(PLUGIN_REF))
+    api_key        = payload.get('apiKey', apiKey.get_for_resource(PLUGIN_REF))
 
-    return JsonResponse({'Response': response.content})
+    response = send_to_slack(channel_name, user_name, message_body, api_key)
+    return JsonResponse({'Response': response})
 
-
-# Needs to be scoped down! Will span slack general channel -Peter
+# Needs to be scoped down! Will span slack channel -Peter
 # @hookpoint('divvycloud.instance.modified')
-# def hamster(resource, old_resource_data, user_resource_id=None):
+# def send_change_to_slack(resource, old_resource_data, user_resource_id=None):
+#     """Send a message to slack if resource has changed."""
+#     #pass searchID into function?
+#     # if(!resource.resourceID == search_ID ):
+#     #     return
+#     #
+#
 #     payload = {
-#         'channel': '#general',
-#         'username': 'annagoldberg',
-#         'text': "this is a test on modified of an instance"
+#         'channel':  channel.get_for_resource(plugin_ref),
+#         'username': username.get_for_resource(plugin_ref),
+#         'text':     'this is a modified instance'
 #     }
 #
 #     data = json.dumps(payload)
 #     return requests.post("https://hooks.slack.com/services/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", data=data)
 
+
+# @BotFunctionRegistry.action(
+#     display_name="Send Slack Message",
+#     unique_name="divvy.send_slack",
+#     input_types=["instance"],
+#     setting_definitions=[
+#         StringField(
+#             name='channel',
+#             display_name='Slack Channel',
+#             description="Slack Channel from which the message should be sent"),
+#         StringField(
+#             name='username',
+#             display_name='Slack Username',
+#             description="Slack Username from which the message should be sent"),
+#         StringField(
+#             name='message',
+#             display_name='Message Body',
+#             description="Contents of the message which should be sent"),
+#         StringField(
+#             name='apiKey',
+#             display_name='Slack API key',
+#             description="Slack API key to allow slack integration"),
+#     ])
+# def slack_message(resource, current_bot, settings):
+#     send_to_slack(channel=settings['channel'].format(resource=resource),
+#                username=settings['username'].format(resource=resource),
+#                message=settings['subject'].format(resource=resource),
+#                apiKey=settings['apiKey'].format(resource=resource),
+#                botFactory=True)
+#     print 'slack message sent!'
 
 def load():
     register_api_blueprint(blueprint)
