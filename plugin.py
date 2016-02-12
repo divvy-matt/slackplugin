ad38249a585ed541c185343ab13436e5d13c4609
Anna Goldberg <annalexgoldberg@gmail.com>
2016-02-12 15:44:51 -0500

    adds hookpoint functionality

--- plugin.py
+++ plugin.py
@@ -25,11 +25,18 @@ class metadata(PluginMetadata):
 blueprint = Blueprint('slack', __name__, static_folder='html', template_folder='html')
 
 #Setting Names
-PREFIX   = DivvyPlugin.get_current_plugin().name
-APIKEY   = PREFIX + '.apiKey'
-CHANNEL  = PREFIX + '.channel'
-USERNAME = PREFIX + '.username'
-MESSAGE  = PREFIX + '.message'
+PREFIX           = DivvyPlugin.get_current_plugin().name
+APIKEY           = PREFIX + '.apiKey'
+CHANNEL          = PREFIX + '.channel'
+USERNAME         = PREFIX + '.username'
+MESSAGE          = PREFIX + '.message'
+RESOURCEID       = PREFIX + '.resourceID'
+RESOURCETYPE     = PREFIX + '.resourceType'
+RESOURCENAME     = PREFIX + '.resourceName'
+RESOURCELOCATION = PREFIX + '.resourceLocation'
+ORIGINID         = PREFIX + '.originID'
+RESOURCETAGKEY   = PREFIX + '.resourceTagKey'
+
 #Plugin reference
 PLUGIN_REF = DivvyPlugin.get_current_plugin()
 
@@ -38,25 +45,67 @@ apiKey = GlobalSetting(
     name         = APIKEY,
     display_name = 'Slack API key',
     type_hint    = 'string',
-    description  = 'This apiKey allows a user access slack integration')
+    description  = 'This apiKey allows a user to access slack integration')
 
 channel = GlobalSetting(
     name         = '#'+CHANNEL,
     display_name = 'Slack channel name',
     type_hint    = 'string',
-    description  = 'This channel is where slack will post messages')
+    description  = 'This is where slack will post messages')
 
 username = GlobalSetting(
     name         = USERNAME,
     display_name = 'Slack username',
     type_hint    = 'string',
-    description  = 'This will be the username that messages are sent from')
+    description  = 'This is the username messages are sent from')
 
 message = GlobalSetting(
     name         = MESSAGE,
-    display_name = 'Messagy body',
+    display_name = 'Message body',
     type_hint    = 'string',
     description  = 'This is a default message body'
+)
+
+resourceID = GlobalSetting(
+    name         = RESOURCEID,
+    display_name = 'Resource ID',
+    type_hint    = 'string',
+    description  = 'The resource ID'
+)
+
+resourceType = GlobalSetting(
+    name         = RESOURCETYPE,
+    display_name = 'Resource type',
+    type_hint    = 'string',
+    description  = 'The resource type'
+)
+
+resourceName = GlobalSetting(
+   name          = RESOURCENAME,
+   display_name  = 'Resource name',
+   type_hint     = 'string',
+   description   = 'The resource name'
+)
+
+resourceTagKey = GlobalSetting(
+   name          = RESOURCETAGKEY,
+   display_name  = 'Resource tag key',
+   type_hint     = 'string',
+   description   = 'Resource tag key'
+)
+
+originID = GlobalSetting(
+    name         = ORIGINID,
+    display_name = 'Origin ID',
+    type_hint    = 'string',
+    description  = 'The origin ID'
+)
+
+resourceLocation = GlobalSetting(
+   name          = RESOURCELOCATION,
+   display_name  = 'Resource location',
+   type_hint     = 'string',
+   description   = 'The resource location'
 )
 
 def send_to_slack(channel,username,message,apiKey):
@@ -85,23 +134,38 @@ def send_slack():
     response = send_to_slack(channel_name, user_name, message_body, api_key)
     return JsonResponse({'Response': response})
 
-# Needs to be scoped down! Will span slack channel -Peter
-# @hookpoint('divvycloud.instance.modified')
-# def send_change_to_slack(resource, old_resource_data, user_resource_id=None):
-#     """Send a message to slack if resource has changed."""
-#     #pass searchID into function?
-#     # if(!resource.resourceID == search_ID ):
-#     #     return
-#     #
-#
-#     payload = {
-#         'channel':  channel.get_for_resource(plugin_ref),
-#         'username': username.get_for_resource(plugin_ref),
-#         'text':     'this is a modified instance'
-#     }
-#
-#     data = json.dumps(payload)
-#     return requests.post("https://hooks.slack.com/services/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", data=data)
+def resource_match(resource):
+    #get user input for resource criteria
+    resource_id       = resourceID.get_for_resource(PLUGIN_REF)
+    resource_location = resourceLocation.get_for_resource(PLUGIN_REF)
+    origin_id         = originID.get_for_resource(PLUGIN_REF)
+    resource_type     = resourceType.get_for_resource(PLUGIN_REF)
+    resource_name     = resourceName.get_for_resource(PLUGIN_REF)
+    resource_tag_key  = resourceTagKey.get_for_resource(PLUGIN_REF)
+
+    return resource.resource_id.to_string()    == resource_id \
+       or resource.instance_id                 == origin_id \
+       or resource.region_name                 == resource_location \
+       or resource.get_resource_name           == resource_name \
+       or resource.get_instance_type()         == resource_type\
+       or resource.get_tag(resource_tag_key)
+
+
+@hookpoint('divvycloud.instance.created')
+@hookpoint('divvycloud.instance.modified')
+def send_change_to_slack(resource, old_resource_data, user_resource_id=None):
+    """Send a message to slack if resource has changed."""
+
+    #user input for slack messaging
+    channel_name   = channel.get_for_resource(PLUGIN_REF)
+    user_name      = username.get_for_resource(PLUGIN_REF)
+    message_body   = 'This is a modified instance of reference:' + resource.resource_id.to_string()
+    api_key        = apiKey.get_for_resource(PLUGIN_REF)
+
+    #if any of the criteria match send a message
+    if resource_match(resource) :
+        send_to_slack(channel_name, user_name, message_body, api_key)
+
 
 
 # @BotFunctionRegistry.action(
